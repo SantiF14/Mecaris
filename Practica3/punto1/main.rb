c96ac2fda9a791ab97f206d9c3e6a3cb92c7a476
Santiago Freccero <santiagofreccero@hotmail.com>
2023-10-30 16:19:58 -0300

    punto 1 terminado

--- Practica3/punto1/main.rb
+++ Practica3/punto1/main.rb
@@ -1,15 +1,37 @@
+
 require_relative 'phone_book'
-require_relative 'contact'
 
-phoneBook = PhoneBook.new
+book = PhoneBook.new
 
-contact1 =  Contact.new('santi', '19-12-1999', 'santi@yahoo.com', '223457778', 'Calle 56')
-contact2 =  Contact.new('mati', '19-11-1995', 'santi1@yahoo.com', '2234572778', 'Calle 57')
-contact3 =  Contact.new('jorge', '19-2-1969', 'santi12@yahoo.com', '2234577378', 'Calle 58')
+loop do 
+    p "---------------------------------"
+    p "Selecciones un numero para realizar una accion"
+    p "1 - Ver todos los contactos"
+    p "2 - Agregar un contacto"
+    p "3 - Editar un contacto"
+    p "4 - Buscar un contacto"
+    p "5 - Exportar a CSV"
+    p "6 - Salir!"
 
-phoneBook.add_contact(contact1)
-phoneBook.add_contact(contact2)
-phoneBook.add_contact(contact3)
+    op = gets.to_i
 
-p phoneBook.find_all_contact
-phoneBook.export_to_csv
+    case op
+    when 1
+        book.show_all_contact
+    when 2
+        book.add_contact
+    when 3
+        book.edit_contact
+    when 4
+        book.find_contact
+    when 5
+        book.export_to_csv
+    when 6
+        break
+    else
+        p "Numero invalido"
+    end
+end
+
+
+
--- Practica3/punto1/phone_book.rb
+++ Practica3/punto1/phone_book.rb
@@ -6,18 +6,75 @@ class PhoneBook
         @contacts = []
     end
 
-    def add_contact(contact)
+    def add_contact()
+        p "Inserte nombre"
+        name = gets.chomp
+        p "Inserte fecha de nacimiento"
+        b_date = gets.chomp
+        p "Inserte email"
+        email = gets.chomp
+        p "Inserte numero de telefono"
+        phone = gets.chomp
+        p "Inserte direccion"
+        address = gets.chomp
+
+        contact = Contact.new(name,b_date,email,phone,address)
+
         @contacts << contact
     end
 
     def find_contact
-        @contacts.each do |contact|
-
+        p "Ingrese numero de identificacion del contacto (numero mayor a 0)"
+        id = gets.to_i
+        id = id -1
+        if id >= 0 && id < @contacts.length
+            p "nombre: #{@contacts[id].name}/ mail #{@contacts[id].email}"
+        else
+            p "No existe un contacto con ese id"
         end
     end
 
-    def find_all_contact
-        @contacts
+    def show_all_contact
+        @contacts.each do |contact|
+            p "nombre: #{contact.name}/ mail #{contact.email}"
+        end
+    end
+
+    def edit_contact
+        p "Ingrese numero de identificacion del contacto"
+        id = gets.to_i
+
+        if id >= 0 && id < @contacts.length
+            p "Inserte nombre (si desea dejar el mismo presione enter)"
+            name = gets.chomp
+            p "Inserte fecha de nacimiento (si desea dejar el mismo presione enter)"
+            b_date = gets.chomp
+            p "Inserte email (si desea dejar el mismo presione enter)"
+            email = gets.chomp
+            p "Inserte numero de telefono (si desea dejar el mismo presione enter)"
+            phone = gets.chomp
+            p "Inserte direccion (si desea dejar el mismo presione enter)" 
+            address = gets.chomp
+
+            contact = @contacts[id]
+            if name != ""
+                contact.name=name
+            end
+            if b_date != ""
+                contact.b_date=b_date
+            end
+            if email != ""
+                contact.email=email
+            end
+            if phone != ""
+                contact.phone=phone
+            end
+            if address != ""
+                contact.address=address
+            end
+        else
+            p "No existe un contacto con ese  id"
+        end
     end
 
     def export_to_csv
